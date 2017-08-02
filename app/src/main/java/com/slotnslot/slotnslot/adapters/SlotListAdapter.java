115a5fc49dd823a15ed561e6d6d2f96faf0e1e0b
razer102938 <razer102938@gmail.com>
2017-08-02 23:46:57 +0900

    Player occupy with deposite when player enter slotroom

--- app/src/main/java/com/slotnslot/slotnslot/adapters/SlotListAdapter.java
+++ app/src/main/java/com/slotnslot/slotnslot/adapters/SlotListAdapter.java
@@ -1,29 +1,44 @@
 package com.slotnslot.slotnslot.adapters;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 
 import com.baoyz.actionsheet.ActionSheet;
 import com.slotnslot.slotnslot.ListType;
+import com.slotnslot.slotnslot.MainApplication;
 import com.slotnslot.slotnslot.R;
 import com.slotnslot.slotnslot.SlotType;
 import com.slotnslot.slotnslot.activities.MakeSlotActivity;
 import com.slotnslot.slotnslot.activities.SlotGameActivity;
+import com.slotnslot.slotnslot.contract.SlotMachine;
 import com.slotnslot.slotnslot.geth.Utils;
+import com.slotnslot.slotnslot.models.PlayerSeed;
 import com.slotnslot.slotnslot.models.SlotRoomViewModel;
 import com.slotnslot.slotnslot.provider.AccountProvider;
 import com.slotnslot.slotnslot.utils.Constants;
+import com.slotnslot.slotnslot.utils.Convert;
 import com.slotnslot.slotnslot.views.SlotImageViewHolder;
 import com.slotnslot.slotnslot.views.SlotMakeViewHolder;
 import com.slotnslot.slotnslot.views.SlotViewHolder;
 
+import org.web3j.abi.datatypes.Bool;
+
 import java.util.ArrayList;
+
+import io.reactivex.Observable;
 
 public class SlotListAdapter extends RecyclerView.Adapter {
     public static final String TAG = SlotListAdapter.class.getSimpleName();
@@ -35,6 +50,7 @@ public class SlotListAdapter extends RecyclerView.Adapter {
     private ArrayList<SlotRoomViewModel> items = new ArrayList<>();
     private ListType type;
     private Fragment fragment;
+    private Double deposite;
 
     public SlotListAdapter(ArrayList<SlotRoomViewModel> items, ListType type, Fragment fragment) {
         this.items = items;
@@ -95,9 +111,36 @@ public class SlotListAdapter extends RecyclerView.Adapter {
                 });
                 holder.itemView.setOnClickListener(view -> {
                     SlotRoomViewModel slotRoomViewModel = items.get(position - (type == ListType.PLAY ? 1 : 0));
-                    enterSlotRoom(slotRoomViewModel);
+                    if (type == ListType.PLAY) {
+                        setDeposite(slotRoomViewModel);
+                    } else {
+                        enterSlotRoom(slotRoomViewModel);
+                    }
                 });
         }
+    }
+
+    private void setDeposite(SlotRoomViewModel viewModel) {
+        Context context = MainApplication.getContext();
+        AlertDialog.Builder builder = new AlertDialog.Builder(fragment.getActivity());
+        EditText editText = new EditText(context);
+        editText.setTextColor(ContextCompat.getColor(context, R.color.pink1));
+        editText.getBackground().setColorFilter(context.getResources().getColor(R.color.pink1), PorterDuff.Mode.SRC_ATOP);
+        FrameLayout container = new FrameLayout(context);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.leftMargin = context.getResources().getDimensionPixelSize(R.dimen.input_deposite_margin);
+        params.rightMargin = context.getResources().getDimensionPixelSize(R.dimen.input_deposite_margin);
+        editText.setLayoutParams(params);
+        container.addView(editText);
+        builder.setView(container);
+        builder.setTitle("Input Deposite");
+        builder.setPositiveButton("OK", (dialogInterface, i) -> {
+            this.deposite = Double.parseDouble(editText.getText().toString());
+            enterSlotRoom(viewModel);
+        });
+        builder.setNegativeButton("CANCEL", (dialogInterface, i) -> {
+        });
+        builder.show();
     }
 
     private void enterSlotRoom(SlotRoomViewModel viewModel) {
@@ -109,6 +152,14 @@ public class SlotListAdapter extends RecyclerView.Adapter {
             intent.putExtras(bundle);
             fragment.getContext().startActivity(intent);
             return;
+        }
+
+        if (type == ListType.PLAY) {
+            SlotMachine machine = viewModel.getRxSlotRoom().getMachine();
+            Observable<Bool> playerSeedReadyObservable = machine.initialPlayerSeedReady().filter(isValid -> isValid.getValue());
+            playerSeedReadyObservable.flatMap(playerSeed ->
+                machine.occupy(new PlayerSeed().getInitialSeed(), Convert.toWei(this.deposite, Convert.Unit.ETHER))
+            ).subscribe();
         }
 
         viewModel
@@ -132,7 +183,7 @@ public class SlotListAdapter extends RecyclerView.Adapter {
                                 Log.i(TAG, "slot machine is already occupied by other user : " + viewModel.getPlayerAddress());
                                 return;
                             }
-
+                            
                             Intent intent = new Intent(fragment.getContext(), SlotGameActivity.class);
                             Bundle bundle = new Bundle();
                             bundle.putSerializable(Constants.BUNDLE_KEY_SLOT_ROOM, viewModel.getSlotAddress());
