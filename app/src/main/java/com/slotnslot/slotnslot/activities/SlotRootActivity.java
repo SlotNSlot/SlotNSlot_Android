c0576f9252d6b8cd3eb7ecedcba1a992be650c43
razer102938 <razer102938@gmail.com>
2017-08-15 23:17:38 +0900

    Add keep to screen on, Fixed prevent click, when game is loading

--- app/src/main/java/com/slotnslot/slotnslot/activities/SlotRootActivity.java
+++ app/src/main/java/com/slotnslot/slotnslot/activities/SlotRootActivity.java
@@ -2,7 +2,9 @@ package com.slotnslot.slotnslot.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.RelativeLayout;
 
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
@@ -14,10 +16,12 @@ public abstract class SlotRootActivity extends RxAppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
     public void setLoadingView(RelativeLayout loadingView) {
         this._loadingView = loadingView;
+        _loadingView.setOnTouchListener((view, motionEvent) -> true);
     }
 
     public void loadingViewSetVisible(boolean isVisible) {
